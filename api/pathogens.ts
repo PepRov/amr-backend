eb0123c37180c1d3dc9d8fec469181ef485788fd
Finios <peptide3dstruct@gmail.com>
2026-05-05 17:58:41 +0100

    Update pathogens.ts

--- api/pathogens.ts
+++ api/pathogens.ts
@@ -1,8 +1,11 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-//import pathogens from '../data/pathogens.json';
 import * as fs from 'fs';
 import * as path from 'path';
 
 export default function (request: VercelRequest, response: VercelResponse) {
+  const filePath = path.join(process.cwd(), 'data', 'pathogens.json');
+  const jsonData = fs.readFileSync(filePath, 'utf-8');
+  const pathogens = JSON.parse(jsonData);
+
   response.status(200).json(pathogens);
 }
