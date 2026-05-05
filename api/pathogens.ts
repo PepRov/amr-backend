9dc39655a7604aa145458fb467f2aa00984d8498
Finios <peptide3dstruct@gmail.com>
2026-05-05 18:20:52 +0100

    Update pathogens.ts

--- api/pathogens.ts
+++ api/pathogens.ts
@@ -1,12 +1,12 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function (request: VercelRequest, response: VercelResponse) {
-  fetch('https://amr-backend.vercel.app/data/pathogens.json')
-    .then(res => res.json())
-    .then(data => {
-      response.status(200).json(data);
-    })
-    .catch(err => {
-      response.status(500).json({ error: 'Failed to load AMR data' });
-    });
+export default async function (request: VercelRequest, response: VercelResponse) {
+  try {
+    const res = await fetch('https://amr-backend.vercel.app/data/pathogens.json');
+    const data = await res.json();
+
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(500).json({ error: 'Failed to load AMR data' });
+  }
 }
